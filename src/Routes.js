7f83166913ceddb61e68b35c75668426047dbbb8
Pratham <prathamk7569@gmail.com>
2024-01-03 17:52:42 +0530

    Show Online Feature

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,15 @@
 import { useContext } from "react";
 import RegisterAndLoginForm from "./RegisterAndLoginForm";
 import { UserContext } from "./UserContext";
+import Chat from "./Chat";
 
 export default function Routes(){
     const {username, id} = useContext(UserContext);
 
-    // console.log("Hello", username)
-
+    console.log("Hello", username)
+    
     if(username){
-        return `Logged in ${username}`
+        return <Chat />
     }
     else
     return(
