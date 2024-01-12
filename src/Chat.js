439c4af1b0497daa084dca60007d6bc02c30d594
Pratham <prathamk7569@gmail.com>
2024-01-12 09:40:43 +0530

    Image sending feature

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,10 @@
-import { useContext, useEffect, useRef, useState } from "react";
+import {useContext, useEffect, useRef, useState} from "react";
 import Avatar from "./Avatar";
 import Logo from "./Logo";
-import { UserContext } from "./UserContext";
+import {UserContext} from "./UserContext.js";
+import {uniqBy} from "lodash";
 import axios from "axios";
 import Contact from "./Contact";
-const {uniqBy} = require('lodash');
 
 export default function Chat() {
   const [ws,setWs] = useState(null);
@@ -54,26 +54,20 @@ export default function Chat() {
       setUsername(null);
     });
   }
-  async function sendMessage(ev, file = null) {
+  function sendMessage(ev, file = null) {
     if (ev) ev.preventDefault();
-  
     ws.send(JSON.stringify({
       recipient: selectedUserId,
       text: newMessageText,
       file,
     }));
-  
     if (file) {
-      try {
-        const res = await axios.get('/messages/' + selectedUserId);
+      axios.get('/messages/'+selectedUserId).then(res => {
         setMessages(res.data);
-        console.log("2");
-      } catch (error) {
-        console.error("Error fetching messages:", error);
-      }
+      });
     } else {
       setNewMessageText('');
-      setMessages(prev => ([...prev, {
+      setMessages(prev => ([...prev,{
         text: newMessageText,
         sender: id,
         recipient: selectedUserId,
@@ -130,7 +124,7 @@ export default function Chat() {
       <div className="bg-white w-1/3 flex flex-col">
         <div className="flex-grow">
           <Logo />
-          {Object.keys(onlinePeopleExclOurUser).map(userId => (
+          {onlinePeople && Object.keys(onlinePeopleExclOurUser).map(userId => (
             <Contact
               key={userId}
               id={userId}
@@ -177,12 +171,16 @@ export default function Chat() {
                       {message.text}
                       {message.file && (
                         <div className="">
-                          <a target="_blank" className="flex items-center gap-1 border-b" href={axios.defaults.baseURL + '/uploads/' + message.file}>
+                          {/* <a target="_blank" className="flex items-center gap-1 border-b" href={'https://res.cloudinary.com/dctsq7knn/image/upload/v1704994122/'+ message.file}>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
                               <path fillRule="evenodd" d="M18.97 3.659a2.25 2.25 0 00-3.182 0l-10.94 10.94a3.75 3.75 0 105.304 5.303l7.693-7.693a.75.75 0 011.06 1.06l-7.693 7.693a5.25 5.25 0 11-7.424-7.424l10.939-10.94a3.75 3.75 0 115.303 5.304L9.097 18.835l-.008.008-.007.007-.002.002-.003.002A2.25 2.25 0 015.91 15.66l7.81-7.81a.75.75 0 011.061 1.06l-7.81 7.81a.75.75 0 001.054 1.068L18.97 6.84a2.25 2.25 0 000-3.182z" clipRule="evenodd" />
                             </svg>
                             {message.file}
-                          </a>
+                          </a> */}
+                          <img className="cursor-pointer"
+                              src={`https://res.cloudinary.com/dctsq7knn/image/upload/v1704994122/${message.file}`}
+                              alt=""
+                          />
                         </div>
                       )}
                     </div>
